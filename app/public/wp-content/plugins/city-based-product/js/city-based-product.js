aa349b002e73dbd645c782030d37526c3d57b265
Deiv-Dev <nomaxas@gmail.com>
2024-05-02 09:00:27 +0300

    refactored checkout to have inputs that is required and removed ones that arent required

--- app/public/wp-content/plugins/city-based-product/js/city-based-product.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function($) {
-    // Wait for the document to be ready
-    $('.woocommerce-product-attributes-item--attribute_kaunas').remove(); // Remove the Kaunas attribute
-});
